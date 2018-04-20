fda7f710734f6925223e8222f3f1e5eacccc5fc4
unknown <benjamin.rademacher@gmail.com>
2018-04-20 12:23:54 -0500

    initial pseudocode for JS and HTML

---
+++ assets/JavaScript/myscripts.js
@@ -0,0 +1,25 @@
+// Global array to hold buttons created for giphy's 
+
+// Global variable to hold information from form (AKA user input)
+
+// JS function for what to do with user input
+
+    // create button element of user request
+
+        // log buttons to make sure functionality is correct
+
+        // buttons should be appended to end of array of buttons (append)
+
+        // buttons created API request
+
+        // onClick of button in array will..
+        
+            // clear previous gifs on page
+            
+            // populate page with 10 gifs
+
+                // include "rating", "title", and "tags" metadata
+
+                // gifs are all stopped on population
+
+                // gifs have start/stop "on click" functionality
